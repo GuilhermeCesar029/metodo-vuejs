45aec89f2aaf16c3ed47431cd181d9ed9302d824
GuilhermeCesar029 <guicesar029@gmail.com>
2020-09-04 22:26:52 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Metodo/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Metodo from './components/Metodo'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Metodo
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+
---
+++ src/components/Metodo.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="metodo">
+        <label>Qual seu nome?</label><br>
+        <input v-model="nomeField" placeholder="Seu nome"><br>
+        <button v-on:click="cumprimentar">Cumprimentar</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        data(){
+            return {
+                nomeField: "",
+            }            
+        },
+        methods: {
+            cumprimentar: function() {
+                alert('olá ' + this.nomeField + '!')
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .metodo {
+        text-align: center;
+    }
+</style>
